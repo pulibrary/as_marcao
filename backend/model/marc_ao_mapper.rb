920805dbb3dde86d00d47a3749339cdd1d06a870
Mark Triggs <mark@dishevelled.net>
2023-03-27 15:46:40 +1100

    Extend XML escaping to all character types

--- backend/model/marc_ao_mapper.rb
+++ backend/model/marc_ao_mapper.rb
@@ -1,4 +1,4 @@
-# This mapping code is adapted from an earlier implmentation found here:
+# This mapping code is adapted from an earlier implementation found here:
 #   https://github.com/pulibrary/aspace_helpers/blob/main/reports/aspace2alma/get_ao2MARC_data.rb
 #
 # The `self.map` method expects a JSONModel rendering of an Archival Object with the following
@@ -22,10 +22,14 @@ class MarcAOMapper
     text.to_s.gsub(%r{</?[\D\S]+?>}, '')
   end
 
-  #literal ampersands are sprinkled throughout; they break the XML
-  #check 6 characters out to make sure it's not already an html entity, and if not, entify it
-  def self.entify_ampersands(text)
-    text.to_s.gsub(/([\w\s])(\&)(?![\w\s]{2,6};)/, '\\1&amp;')
+  def self.xml_escape(text)
+    encoded = text.to_s.encode(:xml => :text)
+
+    # incoming text might have been partially encoded already.  Where we've doubled
+    # up, revert our encoding.
+    #
+    # fish &amp;amp; chips --> fish &amp; chips
+    encoded.gsub(/&([a-z]+?);\1;/, '&\1;')
   end
 
   def self.collection_to_marc(ao_jsons)
@@ -188,7 +192,7 @@ class MarcAOMapper
       else nil
       end
     tag245 = "<datafield ind1=' ' ind2=' ' tag='245'>
-          <subfield code = 'a'>#{entify_ampersands(title)}</subfield>
+          <subfield code = 'a'>#{xml_escape(title)}</subfield>
           #{subfield_f ||= ''}
           </datafield>"
     # addresses github 181 Extents	300
@@ -223,32 +227,32 @@ class MarcAOMapper
     # addresses github 181 'Scope and contents	520'
     tags520 = scope_notes.map do |scope_note|
       "<datafield ind1=' ' ind2=' ' tag='520'>
-            <subfield code = 'a'>#{entify_ampersands(scope_note)}</subfield>
+            <subfield code = 'a'>#{xml_escape(scope_note)}</subfield>
             </datafield>"
     end
     # addresses github 181 'Immediate Source of Acquisition	541'
     tags541 = acq_notes.map do |acq_note|
       "<datafield ind1=' ' ind2=' ' tag='541'>
-              <subfield code = 'a'>#{entify_ampersands(acq_note)}</subfield>
+              <subfield code = 'a'>#{xml_escape(acq_note)}</subfield>
               </datafield>"
     end
     # adds related materials note
     tags544 = related_notes.map do |related_note|
       "<datafield ind1=' ' ind2=' ' tag='544'>
-              <subfield code = 'a'>#{entify_ampersands(related_note)}</subfield>
+              <subfield code = 'a'>#{xml_escape(related_note)}</subfield>
               </datafield>"
     end
     # addresses github 181 '# Agents/Biographical/Historical note	545'
     tags545 = bioghist_notes.map do |bioghist_note|
       "<datafield ind1=' ' ind2=' ' tag='545'>
-              <subfield code = 'a'>#{entify_ampersands(bioghist_note)}</subfield>
+              <subfield code = 'a'>#{xml_escape(bioghist_note)}</subfield>
               </datafield>"
     end
 
     # addresses github 181 'Processing Information	583'
     tags583 = processinfo_notes.map do |processinfo_note|
       "<datafield ind1=' ' ind2=' ' tag='583'>
-              <subfield code = 'a'>#{entify_ampersands(processinfo_note)}</subfield>
+              <subfield code = 'a'>#{xml_escape(processinfo_note)}</subfield>
               </datafield>"
     end
 
@@ -309,7 +313,7 @@ class MarcAOMapper
       tag1xx <<
         if agent['role'] == 'creator'
           "<datafield ind1='#{name_type}' ind2='#{source_code}' tag='1#{tag.to_s[1..2]}'>
-                  <subfield code = 'a'>#{entify_ampersands(name)}#{add_punctuation unless name[-1] =~ /[.,)-]/}</subfield>
+                  <subfield code = 'a'>#{xml_escape(name)}#{add_punctuation unless name[-1] =~ /[.,)-]/}</subfield>
                   #{dates unless agent['name_dates'].nil?}
                   #{subfield_e ||= ''}
                   #{subfield_2 ||= ''}
@@ -317,7 +321,7 @@ class MarcAOMapper
                 </datafield>"
         end
       "<datafield ind1='#{name_type}' ind2='#{tag.to_s[0]=='7' ? ' ' : source_code}' tag='#{tag}'>
-              <subfield code = 'a'>#{entify_ampersands(name)}#{add_punctuation unless name[-1] =~ /[.,)-]/}</subfield>
+              <subfield code = 'a'>#{xml_escape(name)}#{add_punctuation unless name[-1] =~ /[.,)-]/}</subfield>
               #{dates unless agent['name_dates'].nil?}
               #{subfield_e ||= ''}
               #{subfield_2 ||= ''}
