3b7dd4cf5e6e7c0971970942b66a07c82a6a7988
James Bullen <jambun@gmail.com>
2023-03-29 10:27:51 +1100

    only admins can access backend endpoints

--- backend/controllers/ao_marc_mapper.rb
+++ backend/controllers/ao_marc_mapper.rb
@@ -2,7 +2,7 @@ class ArchivesSpaceService < Sinatra::Base
   Endpoint.get('/marcao/export')
     .description("Run the marcao exporter")
     .params()
-    .permissions([])
+    .permissions([:administer_system])
     .returns([200, "status"]) \
   do
     json_response(MarcAOExporter.run)
@@ -11,19 +11,19 @@ class ArchivesSpaceService < Sinatra::Base
   Endpoint.get('/marcao/last_report')
     .description("Get the report from the last marcao exporter run")
     .params()
-    .permissions([])
+    .permissions([:administer_system])
     .returns([200, "report"]) \
   do
     json_response(MarcAOExporter.last_report || {:error => 'no report'})
   end
 
-  Endpoint.get('/repositories/:repo_id/archival_objects/:id/marc')
+  Endpoint.get('/repositories/:repo_id/archival_objects/:id/marcao')
     .description("Get a MARC version of the AO")
     .params(
             ["id", :id],
             ["repo_id", :repo_id],
             )
-    .permissions([])
+    .permissions([:administer_system])
     .returns([200, "MARC"]) \
   do
     json = resolve_references(ArchivalObject.to_jsonmodel(params[:id]), MarcAOMapper.resolves)
@@ -42,7 +42,7 @@ class ArchivesSpaceService < Sinatra::Base
             ["repo_id", :repo_id],
             ["since", String, "Only include AOs modified since the specified date/time", :optional => true]
             )
-    .permissions([])
+    .permissions([:administer_system])
     .returns([200, "MARC"]) \
   do
     ao_ds = ArchivalObject.filter(:root_record_id => params[:id])
