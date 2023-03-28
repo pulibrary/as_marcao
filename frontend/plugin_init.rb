926e4af29bf3f71023315b027b6e305315fcbfbf
James Bullen <jambun@gmail.com>
2023-03-28 13:53:58 +1100

    fleshed out report page

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,1 +1,10 @@
 ArchivesSpace::Application.extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
+
+if AppConfig.has_key?(:marcao_sftp_host) &&
+    AppConfig.has_key?(:marcao_sftp_user) &&
+    AppConfig.has_key?(:marcao_sftp_password) &&
+    AppConfig.has_key?(:marcao_sftp_path)
+  AppConfig[:marcao_sftp_enabled] = true
+else
+  AppConfig[:marcao_sftp_enabled] = false
+end
